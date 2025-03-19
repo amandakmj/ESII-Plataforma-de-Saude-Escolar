9ff7da8dfab903aae8199cbfb8dcdf671d76fc38
Jv-Reis <jreis1112@gmail.com>
2025-03-19 10:13:14 -0300

    Adicionei get_usuario e get_usuario_id

--- backend/app/api/endpoints/usuarios.py
+++ backend/app/api/endpoints/usuarios.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.schemas.usuario import UserCreate
-from app.services.user_services import criar_usuario
+from app.services.user_services import criar_usuario, get_usuario, get_usuario_id
 
 router = APIRouter()
 
 @router.post("/create")
 def criar_novo_usuario(user: UserCreate):
     return criar_usuario(user)
+
+@router.get("/get")
+def pegar_usuario():
+    return get_usuario()
+
+@router.get("/get/{usuario_id}") #só alterei algo aq e em user_services
+def pegar_usuario_id(usuario_id): #verificar se precisar alterar em mais lugar
+    return get_usuario_id(usuario_id) 
+
+# @router.put("update/{usuario_id}")
+# def atualizar_usuario_id (usuario_id, nome, email, senha, tipo_usuario):
+#     return update_usuario_id(usuario_id, nome, email, senha, tipo_usuario)
--- backend/app/services/user_services.py
+++ backend/app/services/user_services.py
@@ -36,3 +36,86 @@ def criar_usuario(usuario):
 # def get_users(db: AsyncSession):
 #     result = db.execute(select(Usuario))
 #     return result.scalars().all()
+
+#Pega todos os usuário do banco
+def get_usuario():
+    conn = connect_db()
+    if not conn:
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco")
+
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT id, nome, email, tipo_usuario FROM usuario;")
+        usuarios = [{"id": u[0], "nome": u[1], "email": u[2], "tipo_usuario": u[3]} for u in cur.fetchall()]
+        cur.close()
+        conn.close()
+        return usuarios
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar usuários: {e}")
+
+#pega o usuário que tem o id que foi passado
+def get_usuario_id(usuario_id: int):
+    conn = connect_db()
+    if not conn:
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco")
+
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT id, nome, email, tipo_usuario FROM usuario WHERE id = %s;", (usuario_id,))
+        usuario = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if usuario:
+            return {"id": usuario[0], "nome": usuario[1], "email": usuario[2], "tipo_usuario": usuario[3]}
+        else:
+            raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar usuário: {e}")
+
+
+# def update_usuario_id(usuario_id: int, nome: str = None, email: str = None, senha: str = None, tipo_usuario: UserTypeEnum = None):
+#     conn = connect_db()
+#     if not conn:
+#         raise HTTPException(status_code=500, detail="Erro ao conectar ao banco")
+
+#     try:
+#         cur = conn.cursor()
+
+#         updates = []
+#         valores = []
+#         if nome:
+#             updates.append("nome = %s")
+#             valores.append(nome)
+#         if email:
+#             updates.append("email = %s")
+#             valores.append(email)
+#         if senha:
+#             senha_hash = hash_senha(senha)
+#             updates.append("senha = %s")
+#             valores.append(senha_hash)
+#         if tipo_usuario:
+#             updates.append("tipo_usuario = %s")
+#             valores.append(tipo_usuario)
+
+#         if not updates:
+#             raise HTTPException(status_code=400, detail="Nenhuma informação para atualizar")
+
+#         valores.append(usuario_id)
+#         query = f"UPDATE usuario SET {', '.join(updates)} WHERE id = %s RETURNING id, nome, email, tipo_usuario;"
+#         cur.execute(query, tuple(valores))
+#         usuario_atualizado = cur.fetchone()
+
+#         conn.commit()
+#         cur.close()
+#         conn.close()
+
+#         if usuario_atualizado:
+#             return {"id": usuario_atualizado[0], "nome": usuario_atualizado[1], "email": usuario_atualizado[2], "tipo_usuario": usuario_atualizado[3]}
+#         else:
+#             raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+#     except Exception as e:
+#         conn.rollback()
+#         raise HTTPException(status_code=500, detail=f"Erro ao atualizar usuário: {e}")
